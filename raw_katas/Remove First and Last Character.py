484c4b305fa55c8447f96a640ae9465c65008761
Théo <106478552+YeQiuST@users.noreply.github.com>
2025-04-15 12:09:48 +0200

    Create Remove First and Last Character.py

---
+++ raw_katas/Remove First and Last Character.py
@@ -0,0 +1,7 @@
+# 🥋 Kata: Remove First and Last Character
+# 🔗 URL: https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+# 🎯 Difficulty: 8 kyu
+# ✍️ Language: Python
+
+def remove_char(s):
+    return s[1:-1]
