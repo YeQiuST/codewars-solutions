aeca23417612b42d6d68768b0972b1b1f84e47a0
Théo <106478552+YeQiuST@users.noreply.github.com>
2025-04-15 12:09:17 +0200

    Create Reversed Strings.py

---
+++ raw_katas/Reversed Strings.py
@@ -0,0 +1,7 @@
+# 🥋 Kata: Reversed Strings
+# 🔗 URL: https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+# 🎯 Difficulty: 8 kyu
+# ✍️ Language: Python
+
+def solution(string):
+    return string[::-1]
