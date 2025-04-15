64147d7ebc1eb28118cbfd43da15fcdb0d8ed801
Théo <106478552+YeQiuST@users.noreply.github.com>
2025-04-15 12:06:23 +0200

    Create Even or Odd.py

---
+++ raw_katas/Even or Odd.py
@@ -0,0 +1,7 @@
+# 🥋 Kata: Even or Odd
+# 🔗 URL: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+# 🎯 Difficulty: 8 kyu
+# ✍️ Language: Python
+
+def even_or_odd(number):
+    return "Even" if number % 2 == 0 else "Odd"
