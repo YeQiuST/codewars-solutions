f85f73439c417d85a6b9f6d709ade191ae0b7370
Théo <106478552+YeQiuST@users.noreply.github.com>
2025-04-15 11:58:25 +0200

    Create For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.py

---
+++ raw_katas/For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.py
@@ -0,0 +1,10 @@
+# 🥋 Kata: For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+# 🔗 URL: https://www.codewars.com/kata/582dafb611d576b745000b74
+# 🎯 Difficulty: 8 kyu
+# ✍️ Language: Python
+
+def quote(fighter):
+    if fighter.lower() == "george saint pierre" : 
+        return "I am not impressed by your performance."
+    elif fighter.lower() == 'conor mcgregor' : 
+        return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
