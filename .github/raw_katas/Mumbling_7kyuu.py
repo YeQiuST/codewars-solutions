d5f33b59eb92bedb3a2d4f50cd265a0ac3825ff9
Théo <106478552+YeQiuST@users.noreply.github.com>
2025-04-15 14:32:17 +0200

    Delete .github/raw_katas directory

--- .github/raw_katas/Mumbling_7kyuu.py
+++
@@ -1,7 +0,0 @@
-# 🥋 Kata: Mumbling
-# 🔗 URL: https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
-# 🎯 Difficulty: 7 kyu
-# ✍️ Language: Python
-
-def accum(st):
-    return '-'.join(j.upper() + j.lower()*i for i, j in enumerate(st))
